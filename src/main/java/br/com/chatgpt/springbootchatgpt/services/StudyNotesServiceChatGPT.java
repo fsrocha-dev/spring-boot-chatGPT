6740b82804ee930cb9a75898eae273a27c5fd14c
Frank Rocha <contato.frankrocha@gmail.com>
2023-04-03 12:59:46 -0300

    chore: add web client call to service

--- src/main/java/br/com/chatgpt/springbootchatgpt/services/StudyNotesServiceChatGPT.java
+++ src/main/java/br/com/chatgpt/springbootchatgpt/services/StudyNotesServiceChatGPT.java
@@ -1,6 +1,8 @@
 package br.com.chatgpt.springbootchatgpt.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.chatgpt.springbootchatgpt.dtos.ChatGPTRequest;
 import br.com.chatgpt.springbootchatgpt.dtos.ChatGPTResponse;
@@ -8,8 +10,24 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class StudyNotesServiceChatGPT {
+
+	private WebClient webClient;
+
+	public StudyNotesServiceChatGPT(
+			WebClient.Builder builder, @Value("${openai.api.key}") String apiKey) {
+		this.webClient = builder
+				.baseUrl("https://api.openai.com/v1/completions")
+				.defaultHeader("Content-Type", "application/json")
+				.defaultHeader("Authorization", String.format("Bearer %s", apiKey))
+				.build();
+	}
+
 	public Mono<ChatGPTResponse> createStudyNotes(String topic) {
 		ChatGPTRequest request = createStudyRequest(topic);
+		return webClient.post()
+				.bodyValue(request)
+				.retrieve()
+				.bodyToMono(ChatGPTResponse.class);
 	}
 
 	public ChatGPTRequest createStudyRequest(String topic) {
